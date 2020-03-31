b2926af1eb8f162141aa8734e1a8ff0495051566
linfeng <linfeng@itcast.cn>
2020-03-31 16:31:15 +0800

    把项目添加到本次Git仓库

---
+++ src/main/java/cn/itcast/git/demo/GitDemo.java
@@ -0,0 +1,12 @@
+package cn.itcast.git.demo;
+
+/**
+ * @author linf
+ * @version 1.0
+ * @date 2020/3/31 16:07
+ */
+public class GitDemo {
+    public static void main(String[] args) {
+        System.out.println("hello world!!");
+    }
+}

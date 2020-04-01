bfcf53f8e7798f52881d2043e1d7e98dda823ed1
linfeng <linfeng@itcast.cn>
2020-04-01 13:48:41 +0800

    master 提交

--- src/main/java/cn/itcast/git/demo/GitDemo.java
+++ src/main/java/cn/itcast/git/demo/GitDemo.java
@@ -9,5 +9,6 @@ public class GitDemo {
     public static void main(String[] args) {
         System.out.println("hello world!!");
         System.out.println("hello git!!");
+        System.out.println("Come on ,Master here");
     }
 }
